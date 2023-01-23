31e2d26a178baeb7902f30ba99ce8631c9e34f37
Vinh Nguyen <vinhnuwin@protonmail.com>
2023-01-23 12:18:15 -0800

    initial structure

---
+++ src/components/App.js
@@ -0,0 +1,22 @@
+import React from "react";
+import PostList from './PostList';
+
+const App = () => {
+    return (
+    <div className="ui container"><PostList /></div>
+    )
+};
+
+export default App;
+
+// import React from "react";
+
+
+// const App = () => {
+//     return (
+//         <div> App
+//         </div>
+//     );
+// };
+
+// export default App;
