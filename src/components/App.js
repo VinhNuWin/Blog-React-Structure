a1736d3ff2276b668e7582e5a662c4cac8e079a1
Vinh Nguyen <vinhnuwin@protonmail.com>
2023-01-24 12:16:38 -0800

    working blog structure

--- src/components/App.js
+++ src/components/App.js
@@ -1,22 +1,12 @@
-import React from "react";
+import React from 'react';
 import PostList from './PostList';
 
 const App = () => {
-    return (
-    <div className="ui container"><PostList /></div>
-    )
+  return (
+    <div className="ui container">
+      <PostList />
+    </div>
+  );
 };
 
 export default App;
-
-// import React from "react";
-
-
-// const App = () => {
-//     return (
-//         <div> App
-//         </div>
-//     );
-// };
-
-// export default App;
--- src/index.js
+++ src/index.js
@@ -12,6 +12,6 @@ const store = createStore(reducers, applyMiddleware(thunk));
 ReactDOM.render(
   <Provider store={store}>
     <App />
-  </Provider>
-  , document.querySelector('#root')
-  );
+  </Provider>,
+  document.querySelector('#root')
+);
